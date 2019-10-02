fe2ab70f5d8f98b16ed9fc1ac4116504711c270d
Ansar <yergeshovansar@gmail.com>
2019-10-03 02:59:47 +0600

    form used by form-tag

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,5 @@
 class User < ApplicationRecord
-    validates :username, presence :true, length: {minimum:5, maximum:25}
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, presence :true, length: {minimum:5}
-                        format: { with: VALID_EMAIL_REGEX },
-                        uniqueness: { case_sensitive: false }
-    validates :password, presence :true, length: {minimum: 5}
+    validates :username, length: {minimum:5, maximum:25}
+    validates :email, presence: true, length: {minimum:5}
+    validates :password, presence: true, length: {minimum: 5}
 end
