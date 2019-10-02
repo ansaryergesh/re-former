cfdf2e923e36901b3a0fc24c5febdf813b4fe0c1
Ansar <yergeshovansar@gmail.com>
2019-10-03 01:34:51 +0600

    validations added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    validates :username, presence :true, length: {minimum:5, maximum:25}
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence :true, length: {minimum:5}
+                        format: { with: VALID_EMAIL_REGEX },
+                        uniqueness: { case_sensitive: false }
+    validates :password, presence :true, length: {minimum: 5}
 end
