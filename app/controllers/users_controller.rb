bcae52d1a60cdf2db405a145330aa9c24fc8b2e8
Ansar <yergeshovansar@gmail.com>
2019-10-04 01:20:38 +0600

    fixing problems

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
 
   def create
-    @user = User.new(user_params)
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
   
     if @user.save
       redirect_to new_user_path
@@ -13,8 +17,8 @@ class UsersController < ApplicationController
     end
   end
 
-  private
-  def user_params
-    params.require(:user).permit(:username, :email, :password)
-  end
+  # private
+  # def user_params
+  #   params.require(:user).permit(:username, :email, :password)
+  # end
 end
